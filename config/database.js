7ca4736aa2e8bf0efde5f01412e838d2639bd45e
S Fujimoto <unicoshun@gmail.com>
2017-12-03 17:52:33 +0900

    second

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-  module.exports = {mongoURI: process.env.mongoURI}
+  module.exports = {mongoURI: 'mongodb://sf:sfujimoto@ds127436.mlab.com:27436/school-prod'}
   
 } else  {
   module.exports = {mongoURI: 'mongodb://localhost/school-dev'}
